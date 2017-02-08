8ba0f7d03081be4fd86a78c6c2ebee3248f94de5
BongKi Cho <whalebab86@gmail.com>
2017-02-08 20:07:48 +0900

    literal, json Report

--- Project/OOP/OOP/main.m
+++ Project/OOP/OOP/main.m
@@ -27,6 +27,15 @@
 #import "FigureVolume.h"
 
 #import "ToolBox.h"
+
+#import "RemindPerson.h"
+#import "Developer.h"
+#import "Designer.h"
+#import "Teacher.h"
+
+#import "PracticeProblem.h"
+
+#import "Iteration.h"
 int main() {
     
     /* WarCraft
@@ -105,45 +114,123 @@ int main() {
     [woman seleftAssetInfo:bank];
     */
     
-    Calculator *cal = [[Calculator alloc] initWithScore:100 withHistory:90 withScience:85 withKorean:95];
+    //Calculator *cal = [[Calculator alloc] initWithScore:100 withHistory:90 withScience:85 withKorean:95];
     
     //NSUInteger addValue = [cal addClassScoreWithMath:100 withHistory:90 withScience:85 withKorean:95];
     //NSUInteger avgValue = [cal avgClassScoreWithMath:100 withHistory:90 withScience:85 withKorean:95];
     
-    NSLog(@"addValue = %lu", [cal addClassScore]);
-    NSLog(@"avgValue = %lu", [cal avgClassScore]);
+//    NSLog(@"addValue = %lu", [cal addClassScore]);
+//    NSLog(@"avgValue = %lu", [cal avgClassScore]);
+//    
+//    FigureArea *area = [[FigureArea alloc] initWithSquareWithS:3];
+//    NSLog(@"정사각형 넓이 %lf", [area squareAreaWithS]);
+//    [area setFigureValueL:8];
+//    NSLog(@"직사각형 넓이 %lf", [area rectangleAreaWithS]);
+//    NSLog(@"원       넓이 %lf", [area circleAreaWithR]);
+//    NSLog(@"삼각형   넓이 %lf", [area triangleAreaWithB]);
+//    NSLog(@"마름모   넓이 %lf", [area trapezoidAreaWithA]);
+//    
+//    FigurePerimeter * perimeter = [[FigurePerimeter alloc] init];
+//    NSLog(@"정사각형 둘레 %lf", [perimeter squarePerimeterWithS]);
+//    NSLog(@"직사각형 둘레 %lf", [perimeter RectanglePerimeterWithL]);
+//    
+//    FigureCircumference * circumference = [[FigureCircumference alloc] init];
+//    NSLog(@"원       둘레 %lf", [circumference circleCirumferenceWithR]);
+//    
+//    FigureVolume * volumn = [[FigureVolume alloc] init];
+//    NSLog(@"정사각체 부피 %lf", [volumn cubeVolumeWithS]);
+//    NSLog(@"직사각체 부피 %lf", [volumn RectangulrSolidVolumeWithL]);
+//    NSLog(@"원통     부피 %lf", [volumn circularVolumeWithR]);
+//    NSLog(@"원형     부피 %lf", [volumn sphereVolumeWithR]);
+//    NSLog(@"뿔       부피 %lf", [volumn coneVolumeWithR]);
+//    
+//    
+//    NSLog(@"%f", [ToolBox inchToCm:3]);
+//    NSLog(@"%f", [ToolBox cmToInch:15]);
+//    
+//    NSLog(@"%f", [ToolBox m2ToPyeong:3]);
+//    NSLog(@"%f", [ToolBox pyeongTom2:15]);
+//    
+//    NSLog(@"%f", [ToolBox fahrenheitToCelsius:3]);
+//    NSLog(@"%f", [ToolBox celsiusToFahrenheit:15]);
     
-    FigureArea *area = [[FigureArea alloc] initWithSquareWithS:3];
-    NSLog(@"정사각형 넓이 %lf", [area squareAreaWithS]);
-    [area setFigureValueL:8];
-    NSLog(@"직사각형 넓이 %lf", [area rectangleAreaWithS]);
-    NSLog(@"원       넓이 %lf", [area circleAreaWithR]);
-    NSLog(@"삼각형   넓이 %lf", [area triangleAreaWithB]);
-    NSLog(@"마름모   넓이 %lf", [area trapezoidAreaWithA]);
+//    RemindPerson *person = [[RemindPerson alloc] initWithName:@"홍길동" withAge:30 withJob:@"개발자" withBirthDay:@"19861018"];
+//    
+//    [person settingWithJob:@"건물주"];
+//    
+//    Boolean resultN = [person isYourBirthDay:@"19871018"];
+//    Boolean resultY = [person isYourBirthDay:@"19861018"];
+//    
+//    NSLog(@"resultN : %hhd", resultN);
+//    NSLog(@"resultY : %hhd", resultY);
+//    
+//    NSLog(@"resultN : %@", person.job);
+//    [person conference];
+//    
+//    Developer *developer =  [[Developer alloc] init];
+//    
+//    developer.job = @"Developer";
+//    
+//    [developer conference];
+//    [developer develop];
+//    [developer test];
+//    
+//    Designer *designer = [[Designer alloc] init];
+//    [designer conference];
+//    [designer design];
+//    [designer confirm];
+//    
+// 
+//    NSInteger result = [Teacher returnLastDayWithMonth:2];
+//    
+//    [Teacher scholarshipWithGrade:2];
+//    
+//    NSLog(@"last Day : %lu", result);
+//    
+//    NSLog(@"50%@", @"%");
+//    
+//    
+//    BOOL developerResult = [Teacher checkDeveloper:developer];
+//    NSLog(@"Developer YN : %hhd", developerResult);
     
-    FigurePerimeter * perimeter = [[FigurePerimeter alloc] init];
-    NSLog(@"정사각형 둘레 %lf", [perimeter squarePerimeterWithS]);
-    NSLog(@"직사각형 둘레 %lf", [perimeter RectanglePerimeterWithL]);
+    // 1번
+//    NSLog(@"%lu", [PracticeProblem absoluteNum:-124]);
+//    NSLog(@"%lu", [PracticeProblem absoluteNum:124]);
+//    
+//    // 2번
+//    NSLog(@"%f", [PracticeProblem roundNum:3]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.1]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.9]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.99]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.999]);
+////    NSLog(@"%f", [PracticeProblem roundNum:-3.999]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.999]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.459]);
+//    NSLog(@"%f", [PracticeProblem roundNum:3.134]);
     
-    FigureCircumference * circumference = [[FigureCircumference alloc] init];
-    NSLog(@"원       둘레 %lf", [circumference circleCirumferenceWithR]);
+    NSLog(@"%f", [PracticeProblem roundNum:3.14515]);
+//    
+//    // 3번
+//    NSLog(@"%lu", [PracticeProblem calcuOp:@"+" withNum1:10 withNum2:3]);
+//    NSLog(@"%lu", [PracticeProblem calcuOp:@"-" withNum1:10 withNum2:3]);
+//    NSLog(@"%lu", [PracticeProblem calcuOp:@"-" withNum1:3 withNum2:10]);
     
-    FigureVolume * volumn = [[FigureVolume alloc] init];
-    NSLog(@"정사각체 부피 %lf", [volumn cubeVolumeWithS]);
-    NSLog(@"직사각체 부피 %lf", [volumn RectangulrSolidVolumeWithL]);
-    NSLog(@"원통     부피 %lf", [volumn circularVolumeWithR]);
-    NSLog(@"원형     부피 %lf", [volumn sphereVolumeWithR]);
-    NSLog(@"뿔       부피 %lf", [volumn coneVolumeWithR]);
+    //[Iteration googoodanFor:3];
+    NSLog(@"%ld", [Iteration factorial:5]);
+    //[Iteration googoodan:3];
+//    NSString *result = [Iteration findMultipleNum:3 maxLange:30];
+//    NSLog(@"%@", result);
+//    
+//    NSInteger resultNum = [Iteration addAllNum:12345];
+//    NSLog(@"%ld", resultNum);
+//    
+//    NSInteger resultIndex = [Iteration findStart:@"*asd*qwe*"];
+//    NSLog(@"%ld", resultIndex);
     
-    
-    NSLog(@"%f", [ToolBox inchToCm:3]);
-    NSLog(@"%f", [ToolBox cmToInch:15]);
-    
-    NSLog(@"%f", [ToolBox m2ToPyeong:3]);
-    NSLog(@"%f", [ToolBox pyeongTom2:15]);
-    
-    NSLog(@"%f", [ToolBox fahrenheitToCelsius:3]);
-    NSLog(@"%f", [ToolBox celsiusToFahrenheit:15]);
-    
+    [Iteration printStartWithCase:1 withNum:3];
+    [Iteration printStartWithCase:2 withNum:3];
+    [Iteration printStartWithCase:3 withNum:3];
+    [Iteration printStartWithCase:4 withNum:3];
+
     return 0;
 }
